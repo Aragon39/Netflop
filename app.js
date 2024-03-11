46190271ddfcb9403447475151725dc5620b100a
Stronmarker <152159818+Stronmarker@users.noreply.github.com>
2024-03-11 16:37:47 +0100

    ajout de la fonction de pagination

--- app.js
+++ app.js
@@ -15,13 +15,18 @@ app.get("/", (req, res) => {
 // Route pour rechercher des films par titre
 app.get("/search", async (req, res) => {
     const searchTerm = req.query.q; // Récupère le terme de recherche depuis les paramètres de requête
+    const page = parseInt(req.query.page) || 1; // Numéro de page, par défaut à la première page
+    const limit = 10; // Nombre de résultats par page
+
     try {
         // Effectuer une requête à l'API OMDB pour récupérer les résultats de recherche
-        const response = await axios.get(`http://www.omdbapi.com/?s=${searchTerm}&apikey=e9025ee9`);
+        const response = await axios.get(`http://www.omdbapi.com/?s=${searchTerm}&apikey=e9025ee9&page=${page}`);
         const movies = response.data.Search; // Récupérer la liste des films à partir de la réponse de l'API
+        const totalResults = parseInt(response.data.totalResults);
+        const totalPages = Math.ceil(totalResults / limit); // Calculer le nombre total de pages
 
         // Rendre le fichier search.ejs avec les données des films
-        res.render("search", { movies: movies });
+        res.render("search", { movies: movies, searchTerm: searchTerm, currentPage: page, totalPages: totalPages });
     } catch (error) {
         console.error("Erreur lors de la recherche de films:", error);
         res.status(500).json({ error: "Erreur lors de la recherche de films" });
