621e7443ddb2bf8829a31849c052202a9168459d
Aragon39 <finielsgaetan@yahoo.fr>
2024-03-11 12:24:33 +0100

    MAJ V0.1

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const app = express();
+const axios = require("axios");
+const express = require("express");
+
+const app = express;
 const port = 3000;
 
-
-
-app.listen(port, () =>{
-    console.log('le sezrver a bien demarrée');
+app.listen(port, () => {
+  console.log(` mon server a bien demarrée ${port}`);
 });
