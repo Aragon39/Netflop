cec16a9c1d367d5dcc489a7ab04b709876aed68b
Stronmarker <152159818+Stronmarker@users.noreply.github.com>
2024-03-11 14:29:00 +0100

    MAJ V0.3, ajout de search.ejs et mofid de app.js

--- app.js
+++ app.js
@@ -4,25 +4,31 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Configuration du moteur de vue EJS
+app.set("view engine", "ejs");
+
+// Route pour afficher la page d'accueil
+app.get("/", (req, res) => {
+    res.render("home"); // Rendre le fichier home.ejs
+});
+
 // Route pour rechercher des films par titre
 app.get("/search", async (req, res) => {
     const searchTerm = req.query.q; // Récupère le terme de recherche depuis les paramètres de requête
     try {
+        // Effectuer une requête à l'API OMDB pour récupérer les résultats de recherche
         const response = await axios.get(`http://www.omdbapi.com/?s=${searchTerm}&apikey=e9025ee9`);
-        res.json(response.data);
+        const movies = response.data.Search; // Récupérer la liste des films à partir de la réponse de l'API
+
+        // Rendre le fichier search.ejs avec les données des films
+        res.render("search", { movies: movies });
     } catch (error) {
         console.error("Erreur lors de la recherche de films:", error);
         res.status(500).json({ error: "Erreur lors de la recherche de films" });
     }
 });
 
-app.set("view engine", "ejs");
-
-app.get("/", (req, res) => {
-    res.render("home"); // Rendre le fichier home.ejs
-});
-
-
+// Démarrage du serveur
 app.listen(port, () => {
     console.log(`Serveur démarré sur le port ${port}`);
 });
