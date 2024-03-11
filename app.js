a4d6bb95175f0d6208af32d198b8e9edfaa0e275
Stronmarker <152159818+Stronmarker@users.noreply.github.com>
2024-03-11 14:06:59 +0100

    MAJ V0.2, modif de app.js & ajout de home.ejs

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const axios = require("axio");
+const axios = require("axios");
 const express = require("express");
 
 const app = express();
 const port = 3000;
+
+// Route pour rechercher des films par titre
+app.get("/search", async (req, res) => {
+    const searchTerm = req.query.q; // Récupère le terme de recherche depuis les paramètres de requête
+    try {
+        const response = await axios.get(`http://www.omdbapi.com/?s=${searchTerm}&apikey=e9025ee9`);
+        res.json(response.data);
+    } catch (error) {
+        console.error("Erreur lors de la recherche de films:", error);
+        res.status(500).json({ error: "Erreur lors de la recherche de films" });
+    }
+});
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home"); // Rendre le fichier home.ejs
+});
+
 
 app.listen(port, () => {
     console.log(`Serveur démarré sur le port ${port}`);
